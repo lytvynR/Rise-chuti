21cf32cac2f01ff1260dcd3ed25121450652901d
LytvynR <Ryslan.2240106@gmail.com>
2017-09-12 01:08:42 +0300

    some js fixes

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ window.onload = function () {
     newGodsSlider();
 }
 
+/*
+    ==================New-goods-slider===================
+    */
+
+
 function newGodsSlider(){
     var elem = document.body;
     var right_arrow = elem.getElementsByClassName('slider_arrow_right')[0];
@@ -13,14 +18,14 @@ function newGodsSlider(){
     var gods = elem.getElementsByClassName('new-goods-slider-item');
     var slider = elem.getElementsByClassName('new-goods-slider')[0];
 
-
-
     left_arrow.addEventListener('click', function(event){
-        slider.innerHTML = '';
-        addNewGoads("hidden", "./images/slider_ico_4.jpg");
+        slider.children[3].outerHTML = "";
+        addNewGoadsLeft("hiddenRI", "./images/slider_ico_3.jpg");
     })
-     right_arrow.addEventListener('click', function(event){
-        addNewGoads("hidden", "./images/slider_ico_4.jpg");
+
+    right_arrow.addEventListener('click', function(event){
+        slider.children[0].outerHTML = "";
+        addNewGoads("hiddenLE", "./images/slider_ico_4.jpg");
     })
 
     window.onresize = resiezeFunc;
@@ -29,46 +34,56 @@ function newGodsSlider(){
         var a = slider.clientHeight;
         a = (a/2)+12 + 'px';
         arrows[0].style.top= a;
-        arrows[1].style.top= a;
-        
+        arrows[1].style.top= a;    
     }
 
     function addNewGoads(goods_name, goods_image){
         slider.innerHTML += '<div class="new-goods-slider-item"><a href="#item"><img src="' + goods_image + '" class="new-goods-slider-item-ico"></a><a href="#item"><div class="new-goods-slider-item-button">' + goods_name + '</div></a></div>'
     }
 
+    function addNewGoadsLeft(goods_name, goods_image){
+        slider.innerHTML = '<div class="new-goods-slider-item"><a href="#item"><img src="' + goods_image + '" class="new-goods-slider-item-ico"></a><a href="#item"><div class="new-goods-slider-item-button">' + goods_name + '</div></a></div>' + slider.innerHTML;
+    }
+
 }
 
-
+/*
+    ==================main-slider===================
+    */
 
 function mainSlider(){
     var elem = document.body;
     var checkbox = elem.getElementsByClassName("checkbox");
     var prew_check = checkbox[0];
-    var slider_background = document.getElementsByClassName("banner-field")[0];
+    var slider_background = document.getElementById("banners-field");
+    var prew_background = slider_background.children[0];
+    
 
     checkbox[0].parentNode.addEventListener('click', function(event) {
         var self = event.target;
         var i = checkbox.length;
 
         if(self.className != 'checkbox'){}else{
-
-            self.className += " checkbox-active";
             var number_of_checkbox = checkTargetNumber(self);
 
+            self.className += " checkbox-active";
             prew_check.className = "checkbox";
             prew_check = self;
-            slider_background.style.backgroundImage = 'url(../images/main_banner_' + number_of_checkbox + '.jpg)';
+
+            slider_background.children[number_of_checkbox].className = "banner-field";
+            prew_background.className += " hidden-banner";
+            prew_background = slider_background.children[number_of_checkbox];
         }
+
+
         function checkTargetNumber(a){
-        
         while(i--){
             if (checkbox[i] == a){
                 var x = i;
                 break
             }
         }
-        return(x+1);
+        return(x);
         }
     });
 
